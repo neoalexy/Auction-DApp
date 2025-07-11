910df998fdbe95e6b2b3fc2608c8be0d6ec90cac
neoalexy <aleksandra.crnatovic@gmail.com>
2025-07-11 20:05:24 +0200

    Update AuctionFactory.sol

--- contracts/AuctionFactory.sol
+++ contracts/AuctionFactory.sol
@@ -1,1 +1,21 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
 
+import "./Auction.sol"; // Pretpostavljamo da je Auction.sol u istom folderu
+
+contract AuctionFactory {
+    Auction[] public auctions;
+
+    function createAuction(uint256 biddingTime, address payable beneficiartAddress, string memory secret) public {
+        Auction newAuction = new Auction(biddingTime, beneficiartAddress, secret);
+        auctions.push(newAuction);
+    }
+
+    function getAllAuctions() public view returns (Auction[] memory) {
+        return auctions;
+    }
+
+    function auctions(uint256 index) public view returns (Auction) {
+        return auctions[index];
+    }
+}
